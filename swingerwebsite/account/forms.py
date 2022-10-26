40d525e5159c9af44452083bd6eac01e3daa6b28
techwithpapito <techwithpapito@gmail.com>
2022-10-26 21:49:01 +0200

    login label fix

--- swingerwebsite/account/forms.py
+++ swingerwebsite/account/forms.py
@@ -1,7 +1,21 @@
 from django import forms 
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
+
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-
+    email = forms.EmailField(label="user_email", required=True)
+    wachtwoord = forms.CharField(label="user_wachtwoord",widget=forms.PasswordInput,required=True)
     
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = UsernameField(
+        label="Gebruikersnaam",
+        widget=forms.TextInput(attrs={"autofocus":True})
+    )
+    
+    password = forms.CharField(
+        label="Wachtwoord",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password"}),
+    )
+
--- swingerwebsite/account/views.py
+++ swingerwebsite/account/views.py
@@ -3,7 +3,16 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from .forms import CustomAuthenticationForm
+
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+
+
+
 
 @login_required
 def dashboard(request):
@@ -12,27 +21,26 @@ def dashboard(request):
                     {"section":"dashboard"})
 
                     
+# def user_login(request):
+#     print("inuser login")
+#     if request.method == 'POST':
+#         print("posting")
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             print("cleaned", cd)
+#             user = authenticate(request,
+#                                 username=cd['username'],
+#                                 password=cd['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request,user)
+#                     return HttpResponse("Authenticate sucessfully")
+#                 else:
+#                     return HttpResponse("Disabled account")
 
-
-
-
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request,user)
-                    return HttpResponse("Authenticate sucessfully")
-                else:
-                    return HttpResponse("Disabled account")
-
-            else:
-                return HttpResponse("Invalid login")
-    else:
-        form = LoginForm()
-    return render(request,"account/login.html",{'form':form})
+#             else:
+#                 return HttpResponse("Invalid login")
+#     else:
+#         form = LoginForm()
+#     return render(request,"account/login.html",{'form':form})
